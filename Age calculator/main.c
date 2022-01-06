517b2961876f4d27b8fa34123ce0e65a1ec0c95c
VAMSIKRISHNA2210 <tt3679@srmist.edu.in>
2022-01-06 11:25:11 +0530

    Initial Commit

---
+++ Age calculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Age calculator
+//
+//  Created by AGENT TIGER on 06/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
